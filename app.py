cb4c201e1f072fbec94647033088f9ad28b7e0e8
tarasovxx <almtara550@gmail.com>
2023-05-23 16:23:03 +0300

    Edited code

--- app.py
+++ app.py
@@ -7,13 +7,6 @@ with open('my_dict.json', encoding='utf-8') as file:
     data = json.load(file)
 
 promotions = list(data)
-# # Пример списка акций
-# promotions = [
-#     {'store': 'Магазин 1', 'title': 'Скидка 10%', 'description': 'Только сегодня'},
-#     {'store': 'Магазин 2', 'title': 'Акция "Купи два, получи скидку"', 'description': 'До конца месяца'},
-#     {'store': 'Магазин 3', 'title': 'Бесплатная доставка', 'description': 'При заказе от 1000 рублей'},
-# ]
-
 
 @app.route('/')
 def index():
@@ -21,4 +14,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
